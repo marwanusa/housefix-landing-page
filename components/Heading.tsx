6f39c83a7e7472333c5e8119bd480be38f6f2d4b
marwanusa <professionalma42@gmail.com>
2025-08-21 16:28:54 +0300

    fix: Correct path for ArrowRight.svg in Heading component

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -7,7 +7,7 @@ const Heading = ({ title, head }: { title: string; head: string }) => {
       <div className="flex gap-2 justify-center items-center">
       <h5 className="h5 font-medium text-primary-500 ">{title}</h5>
       <Image
-      src={"../public/assets/icons/ArrowRight.svg"}
+      src={"/assets/icons/ArrowRight.svg"}
       width={38}
       height={16}
       alt="arrow image"
